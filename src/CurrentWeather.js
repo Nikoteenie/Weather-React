ee256ff038fd393beb6087231af9dd3f6e18ef59
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-04 11:13:14 -0400

    Updated Api Output

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -8,7 +8,7 @@ export default function SearchEngine() {
   let [weather, setWeather] = useState({});
 
   function showWeather(response) {
-    setLoaded (true);
+    setLoaded(true);
     setWeather({
       degrees: response.data.main.temp,
       description: response.data.weather[0].description,
@@ -30,128 +30,127 @@ export default function SearchEngine() {
     setCity(event.target.value);
   }
 
-
-  let form = ( <form id="search-form" className="mb-3" onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search a city.."
-              className="form-control"
-              id="city-input"
-              autoComplete="off"
-              onChange = {updateCity}
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-);
-
-if (loaded) {
-  return (
- <div>
-     {form}
-      <div className="overview">
-        <h1 id="city">{city}</h1>
-        <ul>
-          <li id="date">Last updated: 08/02/2022</li>
-          <li id="description">{weather.description}</li>
-        </ul>
-      </div>
+  let form = (
+    <form id="search-form" className="mb-3" onSubmit={handleSubmit}>
       <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src={SunImage}
-              alt="Clear"
-              id="icon"
-              width="80"
-              className="float-left"
-            />
-            <div className="float-left">
-              <strong id="temperature">{weather.temperature}</strong>
-              <span className="units">
-                <a href="/" id="celsius-link" className="active">
-                  °C
-                </a>{" "}
-                |{" "}
-                <a href="/" id="fahrenheit-link">
-                  °F{" "}
-                </a>{" "}
-              </span>
-            </div>
-          </div>
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Search a city.."
+            className="form-control"
+            id="city-input"
+            autoComplete="off"
+            onChange={updateCity}
+          />
         </div>
-        <div className="col-6">
-          <ul>
-            <li>
-              Humidity: <span id="humidity">{weather.humidity}</span>%
-            </li>
-            <li>
-              Wind: <span id="wind">{weather.wind} </span>
-              km/h
-            </li>
-          </ul>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
         </div>
       </div>
-    </div>
+    </form>
   );
-} else {
-return (
-<div>
-{form} 
-   <div className="overview">
-        <h1 id="city">Lisbon</h1>
-        <ul>
-          <li id="date">Last updated: 08/02/2022</li>
-          <li id="description"> Clear Sky</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src={SunImage}
-              alt="Clear"
-              id="icon"
-              width="80"
-              className="float-left"
-            />
-            <div className="float-left">
-              <strong id="temperature">90</strong>
-              <span className="units">
-                <a href="/" id="celsius-link" className="active">
-                  °C
-                </a>{" "}
-                |{" "}
-                <a href="/" id="fahrenheit-link">
-                  °F{" "}
-                </a>{" "}
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="col-6">
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <div className="overview">
+          <h1 id="city">{city}</h1>
           <ul>
-            <li>
-              Humidity: <span id="humidity">50%</span>%
-            </li>
-            <li>
-              Wind: <span id="wind">5 </span>
-              km/h
-            </li>
+            <li id="date">Last updated: 08/02/2022</li>
+            <li id="description">{weather.description}</li>
           </ul>
         </div>
-    </div>
-    </div>
-);
-}
-  
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={SunImage}
+                alt="Clear"
+                id="icon"
+                width="80"
+                className="float-left"
+              />
+              <div className="float-left">
+                <strong id="temperature">{Math.round(weather.degrees)}</strong>
+                <span className="units">
+                  <a href="/" id="celsius-link" className="active">
+                    °C
+                  </a>{" "}
+                  |{" "}
+                  <a href="/" id="fahrenheit-link">
+                    °F{" "}
+                  </a>{" "}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>
+                Humidity: <span id="humidity">{weather.humidity}</span>%
+              </li>
+              <li>
+                Wind: <span id="wind">{Math.round(weather.wind)} </span>
+                km/h
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {form}
+        <div className="overview">
+          <h1 id="city">Lisbon</h1>
+          <ul>
+            <li id="date">Last updated: 08/02/2022</li>
+            <li id="description"> Clear Sky</li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={SunImage}
+                alt="Clear"
+                id="icon"
+                width="80"
+                className="float-left"
+              />
+              <div className="float-left">
+                <strong id="temperature">90</strong>
+                <span className="units">
+                  <a href="/" id="celsius-link" className="active">
+                    °C
+                  </a>{" "}
+                  |{" "}
+                  <a href="/" id="fahrenheit-link">
+                    °F{" "}
+                  </a>{" "}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>
+                Humidity: <span id="humidity">50%</span>%
+              </li>
+              <li>
+                Wind: <span id="wind">5 </span>
+                km/h
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
